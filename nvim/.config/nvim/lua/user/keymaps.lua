27189be59137b0ed1a0035a46805c282b1074cbe
Lucas Marín <lamarin@uc.cl>
2022-07-19 11:50:49 -0400

    add keymappings

---
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,53 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", ",", "<Nop>", opts) -- to prevent any previous mapping on the leader key
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+-- Modes normal_mode = "n", insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+function _G.ReloadConfig()
+  for name,_ in pairs(package.loaded) do
+    if name:match('^user') then
+      package.loaded[name] = nil
+    end
+  end
+
+  dofile(vim.env.MYVIMRC)
+end
+
+keymap("n", "<leader>e", ":Lex 30<CR>", opts)
+keymap("n", "<leader>y", '"+y', opts)
+keymap("n", "<leader>so", '<Cmd>lua ReloadConfig()<CR>', { noremap = true })
+
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+keymap("v", "<leader>y", '"+y', opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>gv-gv", opts)
+keymap("v", "<A-k>", ":m .-2<CR>gv-gv", opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "<a-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<a-k>", ":move '<-2<CR>gv-gv", opts)
