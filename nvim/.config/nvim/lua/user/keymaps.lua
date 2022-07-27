28660101bfbcecad4c880be4e99323e6d25e031e
Lucas Marín <lamarin@uc.cl>
2022-07-26 22:10:55 -0400

    feat(nvim): add telescope and hls mappings

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -4,7 +4,7 @@
 -- ===== NOTES =====
 -- <C-f>: Edit command in comand line
 -- :h cmdline-editing - To see how navigate the vim command line
--- For regex searches, every special operator has to be scaped, which is the opposite to normal regex, where the characters are scaped and not the operators. 
+-- For regex searches, every special operator has to be scaped, which is the opposite to normal regex, where the characters are scaped and not the operators.
 
 local opts = { noremap = true, silent = true }
 
@@ -26,13 +26,13 @@ vim.g.maplocalleader = ","
 
 -- acá tengo que revisar como se hace lo mismo con librería plenary, una referencia son las dotfiles de teej
 function _G.ReloadConfig()
-  for name,_ in pairs(package.loaded) do
-    if name:match('^user') then
-      package.loaded[name] = nil
-    end
-  end
-
-  dofile(vim.env.MYVIMRC)
+	for name, _ in pairs(package.loaded) do
+		if name:match("^user") then
+			package.loaded[name] = nil
+		end
+	end
+	print("Sourced init.lua")
+	dofile(vim.env.MYVIMRC)
 end
 
 keymap("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>", opts)
@@ -40,12 +40,15 @@ keymap("n", "<leader>p", '"_dP', opts) -- paste into null register to avoid to a
 keymap("n", "<leader>y", '"+y', opts)
 keymap("n", "<leader>o", "o<Esc>", opts)
 keymap("n", "<leader>O", "O<Esc>", opts)
-keymap("n", "<leader>so", '<Cmd>lua ReloadConfig()<CR>', { noremap = true })
+keymap("n", "<leader>H", "<Cmd>:set hls<CR>", opts)
+keymap("n", "<leader>h", "<Cmd>:nohls<CR>", opts)
+keymap("n", "<leader>so", "<Cmd>lua ReloadConfig()<CR>", { noremap = true })
+
 -- Telescope
-keymap("n", "ff", "<Cmd>lua require('telescope.builtin').find_files()<CR>", opts)
-keymap("n", "fg", "<Cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
-keymap("n", "fb", "<Cmd>lua require('telescope.builtin').buffers()<CR>", opts)
-keymap("n", "fh", "<Cmd>lua require('telescope.builtin').help_tags()<CR>", opts)
+keymap("n", "<leader>ff", "<Cmd>lua require('telescope.builtin').find_files()<CR>", opts)
+keymap("n", "<leader>fg", "<Cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
+keymap("n", "<leader>fb", "<Cmd>lua require('telescope.builtin').buffers()<CR>", opts)
+keymap("n", "<leader>fh", "<Cmd>lua require('telescope.builtin').help_tags()<CR>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
