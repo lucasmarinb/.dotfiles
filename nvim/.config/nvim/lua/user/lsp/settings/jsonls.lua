8d2c9119762f7d52a6762d48ad91df3b237f73b6
Lucas Marín <lamarin@uc.cl>
2023-07-16 04:50:01 -0400

    feat: update jsonls to allow formatting

--- nvim/.config/nvim/lua/user/lsp/settings/jsonls.lua
+++ nvim/.config/nvim/lua/user/lsp/settings/jsonls.lua
@@ -1,23 +1,23 @@
 local status_ok, schemastore = pcall(require, "schemastore")
 if not status_ok then
-	return
+  return
 end
 
 local function setup(on_attach, capabilities)
-	require("lspconfig").jsonls.setup({
-		init_options = {
-			provideFormatter = false,
-		},
-		settings = {
-			json = {
-				schemas = schemastore.json.schemas(),
-			},
-		},
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
+  require("lspconfig").jsonls.setup({
+    init_options = {
+      provideFormatter = true,
+    },
+    settings = {
+      json = {
+        schemas = schemastore.json.schemas(),
+      },
+    },
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
 end
 
 return {
-	setup = setup,
+  setup = setup,
 }
